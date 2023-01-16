c33b1e7d89037dbac2cda201ed44d3477d733693
suzhengquan <zelda_ok@163.com>
2023-01-16 12:53:16 +0800

    update

--- include/core/NiiHighLevelGpuProgram.h
+++ include/core/NiiHighLevelGpuProgram.h
@@ -33,6 +33,32 @@ Licence: commerce(www.niiengine.com/license)(Three kinds)
 
 namespace NII
 {
+    /** 高层着色程序工厂类
+    @version NIIEngine 3.0.0
+    */
+    class _EngineAPI HighLevelGpuProgramFactory : public FactoryObj
+    {
+        friend class HighLevelGpuProgramManager;
+    public:
+        HighLevelGpuProgramFactory(HighLevelGpuProgramManager * mag);
+        virtual ~HighLevelGpuProgramFactory();
+
+        /// @copydetails FactoryObj::getVFSType
+        virtual Nmark getVFSType() const;
+
+        /** 语言类型
+        @version NIIEngine 3.0.0
+        */
+        virtual ShaderLanguage getLanguage() const = 0;
+
+        /** 创建着色程序
+        @version NIIEngine 3.0.0
+        */
+        virtual HighLevelGpuProgram * create(ResourceID rid, GroupID gid) = 0;
+    protected:
+        HighLevelGpuProgramManager * mManager;
+    };
+    
     /** 高层着色程序
     @version NIIEngine 3.0.0
     */
@@ -73,9 +99,6 @@ namespace NII
 
         /// @copydetails GpuProgram::getMain
         GpuProgram * getMain();
-
-        /// Scan the source for \#include and replace with contents from OGRE resources
-        static VString _resolveIncludes(const VString& source, Resource* res, const String& fileName);
     protected:
         /** 加载系统级代码实现
         @version NIIEngine 3.0.0
@@ -107,6 +130,11 @@ namespace NII
 
         /// @copydetails Resource::unloadImpl
         void unloadImpl();
+        
+        /**
+        @version NIIEngine 6.0.0
+        */
+        static VString genRef(const VString & code, Resource * res, const String & resfileName);
 
         /// in-situ parsing of defines
         static std::vector<std::pair<const char*, const char*>> parseDefines(VString & defines);
@@ -115,6 +143,172 @@ namespace NII
         VString mPreprocessorDefines;
         bool mGenParamDef;
         bool mSysCodeValid;
+    };
+    
+    /** 转换着色程序
+    @version NIIEngine 3.0.0
+    */
+    class _EngineAPI ProxyGpuProgram : public HighLevelGpuProgram
+    {
+    public:
+        ProxyGpuProgram(ResourceID rid, GroupID gid,
+            ResLoadScheme * ls = 0, ResResultScheme * rs = 0, LangID lid = N_PrimaryLang);
+
+        ~ProxyGpuProgram();
+
+        /** 添加备用着色程序
+        @version NIIEngine 3.0.0
+        */
+        void addCandidate(ResourceID rid);
+
+        /** 移去所有备用着色程序
+        @version NIIEngine 3.0.0
+        */
+        void removeAllCandidate();
+
+        /** 获取当前备用着色程序
+        @version NIIEngine 3.0.0
+        */
+        const HighLevelGpuProgram * getCurrent() const;
+
+        /// @copydetails ResourceID::load
+        void load();
+
+        /// @copydetails ResourceID::reload
+        void reload();
+
+        /// @copydetails ResourceID::unload
+        void unload();
+
+        /// @copydetails ResourceID::touch
+        void touch(bool load = true);
+
+        /// @copydetails ResourceID::untouch
+        void untouch();
+
+        /// @copydetails ResourceID::isLoading
+        bool isLoading() const;
+
+        /// @copydetails ResourceID::isReloadable
+        bool isReloadable() const;
+
+        /// @copydetails ResourceID::getState
+        LoadingState getState() const;
+
+        /// @copydetails ResourceID::getPrc
+        PrcID getPrc() const;
+
+        /// @copydetails ResourceID::getOriginID
+        ResourceID getOriginID() const;
+        
+        /// @copydetails ResourceID::getSize
+        NCount getSize() const;
+
+        /// @copydetails ResourceID::getOriginSize
+        NCount getOriginSize() const;
+
+        /// @copydetails ResourceID::setLoadScheme
+        void setLoadScheme(ResLoadScheme * rls);
+
+        /// @copydetails ResourceID::getLoadScheme
+        ResLoadScheme * getLoadScheme() const;
+
+        /// @copydetails ResourceID::setResultScheme
+        void setResultScheme(ResResultScheme * rrs);
+
+        /// @copydetails ResourceID::getResultScheme
+        ResResultScheme * getResultScheme() const;
+
+        /// @copydetails ResourceID::add
+        void add(ResourceListener * lis);
+
+        /// @copydetails ResourceID::remove
+        void remove(ResourceListener * lis);
+        
+        /// @copydetails GpuProgram::getLanguage
+        ShaderLanguage getLanguage() const;
+
+        /// @copydetails GpuProgram::createParam
+        GpuProgramParam * createParam();
+
+        /// @copydetails GpuProgram::getMain
+        GpuProgram * getMain();
+
+        /// @copydetails GpuProgram::isValid
+        bool isValid() const;
+
+        /// @copydetails GpuProgram::isVertexMatrixFusion
+        bool isVertexMatrixFusion() const;
+
+        /// @copydetails GpuProgram::isVertexInterpolation
+        bool isVertexInterpolation() const;
+
+        /// @copydetails GpuProgram::getVertexOffsetCount
+        NCount getVertexOffsetCount() const;
+
+        /// @copydetails GpuProgram::isVertexTextureFetch
+        bool isVertexTextureFetch() const;
+
+        /// @copydetails GpuProgram::createDefaultParam
+        GpuProgramParam * createDefaultParam();
+
+        /// @copydetails GpuProgram::getDefaultParam
+        GpuProgramParam * getDefaultParam() const;
+
+        /// @copydetails GpuProgram::isEnvColourEnable
+        bool isEnvColourEnable() const;
+
+        /// @copydetails GpuProgram::isEnvLightEnable
+        bool isEnvLightEnable() const;
+
+        /// @copydetails GpuProgram::isEnvFogEnable
+        bool isEnvFogEnable() const;
+
+        /// @copydetails GpuProgram::isEnvSpaceEnable
+        bool isEnvSpaceEnable() const;
+
+        /// @copydetails GpuProgram::getErrorCode
+        Nui32 getErrorCode() const;
+    protected:
+        /// @copydetails GpuProgram::init
+        bool initCmd(PropertyCmdSet * dest);
+
+        /// @copydetails HighLevelGpuProgram::genAssemblerImpl
+        void genAssemblerImpl();
+
+        /// @copydetails HighLevelGpuProgram::unloadSysCodeImpl
+        void unloadSysCodeImpl();
+
+        /// @copydetails HighLevelGpuProgram::genParamDefImpl
+        void genParamDefImpl();
+
+        /// @copydetails GpuProgram::genParamDefImpl
+        void loadCodeImpl(const VString & code);
+    protected:
+        ResourceIDList mCandidateList;
+        HighLevelGpuProgram * mCurrent;
+    };
+
+    /** 转换着色程序工厂类
+    @version NIIEngine 3.0.0
+    */
+    class AlterGpuProgramFactory : public HighLevelGpuProgramFactory
+    {
+    public:
+        AlterGpuProgramFactory(HighLevelGpuProgramManager * mag);
+        ~AlterGpuProgramFactory();
+
+        /// @copydetails FactoryObj::getID
+        FactoryID getID() const;
+
+        /// @copydetails HighLevelGpuProgramFactory::getLanguage
+        ShaderLanguage getLanguage() const;
+
+        /// @copydetails HighLevelGpuProgramFactory::create
+        HighLevelGpuProgram * create(ResourceID rid, GroupID gid);
+
+        /// @copydetails FactoryObj::destroy
+        void destroy(void * prog);
     };
 }
 #endif
